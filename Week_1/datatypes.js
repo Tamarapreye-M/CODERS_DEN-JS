db1fa52791d61ef656a03f07947af45f99d14f32
Tamarapreye-M <preyemichael116@gmail.com>
2022-06-30 18:38:37 +0100

    week 1 assignment

---
+++ Week_1/datatypes.js
@@ -0,0 +1,40 @@
+
+console.log(typeof 'John');
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+let goesToWork;
+let favoritePet;
+let favoriteIcecreamFlavor;
+let mySkinColor;
+
+
+let gender = "female";
+let heterosexual = true;
+let worstIcecreamFlavours = ['vanilla', 'strawberry', 'marshmellows'];
+let boyfriend = {
+    race: 'black',
+    skinColor: 'light skinned',
+    short: false,
+    qualities: ['understanding', 'sweet', 'kind', 'confident']
+
+}
+
+const firstName = 'Tamara';
+const lastName = 'Michael';
+const isMarried = false;
+const country = 'Nigeria';
+const age = 22;
+
+const name= 'Tamara', surname = 'Michael', maritalStatus = 'single', nationality = 'Nigerian', currentAge = 22;
+
+let myAge, yourAge;; 
+myAge = 22;
+yourAge = 24;
+
+console.log(`I am ${myAge} years old. `);
+console.log(`You are ${yourAge} years old. `);
+
+
---
+++ Week_1/variable.js
@@ -0,0 +1,11 @@
+// comments can make code readable
+//  Welcome to 30DaysOfJavaScript
+
+/** comments can make code readable, easy to reuse and informative */
+ 
+let firstName = "John";
+let isMarried = true;
+let lastName = undefined;
+let job = null;
+
+console.log(typeof firstName)
